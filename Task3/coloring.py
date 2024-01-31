5bc49a511a0c69abe95da1d7904478cdff913b3d
datdang2712 <100905322+datdang2712@users.noreply.github.com>
2024-01-31 15:53:45 +0700

    Update coloring.py

--- Task3/coloring.py
+++ Task3/coloring.py
@@ -1,3 +1,7 @@
+import os
+import pandas
+import numpy as np
+
 dirs = [[0, -1], [-1, 0], [0,1], [1, 0], [-1, -1], [-1, 1], [1, 1], [1, -1]]
 def print_grid(grid):
     for row in grid:
@@ -22,17 +26,44 @@ def color(n, m, grid):
     colors = coloring.count(True)
     print(colors)
     print_grid(color_grid)
+
 def main():
-    inFile = './sample1.inp'
-    f = open(inFile, 'r')
-    line = f.readline()
-    n, m, k = [int(x) for x in line.split()]
-    grid = [[0] * m for _ in range(n)]
-    for ii in range(k):
+    listInp = []
+    for dirname, _, filenames in os.walk('./sample/'):
+        for filename in filenames:
+            if filename[-3:]  == 'inp':
+                listInp.append(os.path.join(dirname, filename))
+    listInp = sorted(listInp)
+
+    for idx, filename in enumerate(listInp):
+        '''
+        this is read file template
+        '''
+        input_file = filename
+        counter = filename.split('.')[1].split('/')[-1]
+        counter = int(counter[6:])
+
+
+        inFile = filename
+        if not os.path.exists('./result'):
+            os.mkdir('./result')
+        
+        outFile = f'./result/sample{counter}.out'
+
+        f = open(inFile, 'r')
         line = f.readline()
-        xx, yy = [int(x) for x in line.split()]
-        grid[xx][yy] = 1
-    color(n, m, grid)
+        n, m, k = [int(x) for x in line.split()]
+        listBlocks = []
+        grid = [[0] * m for _ in range(n)]
+        for ii in range(k):
+            line = f.readline()
+            xx, yy = [int(x) for x in line.split()]
+            grid[xx][yy] = 1
+
+        lline = f.readline()
+        sx, sy, ex, ey = [int(x) for x in lline.split()]
+        grid[sx][sy] = 2
+        grid[ex][ey] = 2
+        color(n, m, grid)
 
 main()
-
